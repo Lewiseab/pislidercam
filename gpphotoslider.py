fde69ab3503da5a0aabcb8f8d82c5799eb912c2e
Lewis Aburrow <laburrow@gmail.com>
2015-10-06 18:15:02 +0000

    Works out remaining seconds and minutes correctly

--- gpphotoslider.py
+++ gpphotoslider.py
@@ -17,13 +17,16 @@ GPIO.setup(coil_B_2_pin, GPIO.OUT)
 def backwards(delay, steps, photos, shutter, photorun,bulb):
 	while photos >= 1:
 		time.sleep(1)
-		os.system("gphoto2 --capture-image-and-download --filename %d%m%y-%H%M%S")
+		os.system("gphoto2 --capture-image-and-download --filename %d%m%y-%H%M%S") #takes the photo, downloads it and time/date stamps it
 		photos = photos - 1
 		print "\n" * 60 #Used to clear the console
 		print "There are",photos, "photo(s) remaining to be shot of the",photorun, "you asked for."
 		print "\n"
-		remtime = ((shutter+2)*photos)/60 #Used to clauclate remianing time in minutes - needs to work out seconds with special division
-		print "Bear with me, this will take roughly" ,remtime, "minute(s) to finish..."
+
+		remtime = ((shutter+2)*photos) #Used to clauclate remianing time in seconds
+		remtime_mins = int(remtime/60)
+		remtime_secs = remtime%60
+		print "Bear with me, this will take roughly" ,remtime_mins, "minute(s) and" ,remtime_secs ,"seconds to finish..."
 		for i in range(0, steps):
 			setStep(1, 0, 0, 1)
 			time.sleep(delay)
@@ -41,9 +44,9 @@ def setStep(w1, w2, w3, w4):
 	GPIO.output(coil_B_2_pin, w4)
 
 while True:
-	delay = raw_input("Delay between steps (milliseconds)?")
+	delay = 8 #raw_input("Delay between steps (milliseconds)?")
 	photos = raw_input("How many photos to take?")
 	shutter = raw_input("How long is your shutter speed (seconds)?")
-	steps = raw_input("How many steps backwards for each shot?")
+	steps = raw_input("Move how far between shots?")
 	photorun = photos
 	backwards(int(delay) / 1000.0, int(steps), int(photos), int(shutter) + 1, int(photorun),bulb=0.5)
