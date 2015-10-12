96d172320904d30a14332caff62dfe136a6f9746
Lewis Aburrow <laburrow@gmail.com>
2015-10-12 18:17:26 +0000

    commit before trying to program buttons

--- gpphotoslider.py
+++ gpphotoslider.py
@@ -1,6 +1,18 @@
 import RPi.GPIO as GPIO
 import time
 import os
+import Adafruit_CharLCD as LCD
+
+# Initialize the LCD using the pins
+lcd = LCD.Adafruit_CharLCDPlate()
+
+lcd.set_color(1.0, 1.0, 1.0)
+lcd.clear()
+lcd.message(" --= Lewis' =--  \n --= Slider =--")
+
+
+
+#set the pins for the motor
 GPIO.setmode(GPIO.BCM)
 shutter_pin = 18
 coil_A_1_pin = 4
@@ -27,6 +39,9 @@ def backwards(delay, steps, photos, shutter, photorun,bulb):
 		remtime_mins = int(remtime/60)
 		remtime_secs = remtime%60
 		print "Bear with me, this will take roughly" ,remtime_mins, "minute(s) and" ,remtime_secs ,"seconds to finish..."
+		lcd.clear()
+		lcd_rem_time = str(remtime_mins) + ' minutes and \n' + str(remtime_secs) + ' second(s).'
+		lcd.message(str(lcd_rem_time))
 		for i in range(0, steps):
 			setStep(1, 0, 0, 1)
 			time.sleep(delay)
@@ -59,8 +74,10 @@ while True:
 	shutterspeed_line=f.readlines()
 	shutter_speed =  shutterspeed_line[2]
 	shutter = int(shutter_speed[9:]) + 2
-	print shutter
 
 	steps = raw_input("Move how far between shots?")
+        lcd.clear()
+        lcd.message("Let's begin....")
+        lcd.set_color(0.0, 1.0, 0.0)
 	photorun = photos
-	backwards(int(delay) / 1000.0, int(steps), int(photos), int(shutter) + 1, int(photorun),bulb=0.5)
+	backwards(int(delay) / 1000.0, int(steps), int(photos), float(shutter) + 1, int(photorun),bulb=0.5)
